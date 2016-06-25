ead9c25611eca0afe37e64848179f7b8fc7aeac3
Zeqi Cui <cuizeqiw@gmail.com>
2016-06-25 14:46:26 -0400

    google speech

--- app/src/main/java/bonfirestudio/realtalk/MainActivity.java
+++ app/src/main/java/bonfirestudio/realtalk/MainActivity.java
@@ -1,20 +1,32 @@
 package bonfirestudio.realtalk;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.view.MotionEvent;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtSpeechInput;
+    private ImageButton btnSpeak;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button button = (Button) findViewById(R.id.listenButton);
-
 
         button.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -25,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this, "this is my Toast message!!! =)",
                                 Toast.LENGTH_SHORT).show();
 
+                        promptSpeechInput();
+
                         return true; // if you want to handle the touch event */
                     case MotionEvent.ACTION_UP:
                         Toast.makeText(MainActivity.this,"PROCESSING",Toast.LENGTH_SHORT).show();
@@ -34,10 +48,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
-
     }
+
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtSpeechInput.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+       // getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
 }
+
