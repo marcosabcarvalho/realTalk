fa8a4f4d48ab4f58fde19651f29c5f2630497b1a
Zeqi Cui <cuizeqiw@gmail.com>
2016-06-25 14:32:35 -0400

    -added button with actions on the ontouch

--- app/src/main/java/bonfirestudio/realtalk/MainActivity.java
+++ app/src/main/java/bonfirestudio/realtalk/MainActivity.java
@@ -2,6 +2,10 @@ package bonfirestudio.realtalk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = (Button) findViewById(R.id.listenButton);
+
+
+        button.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+
+                        Toast.makeText(MainActivity.this, "this is my Toast message!!! =)",
+                                Toast.LENGTH_SHORT).show();
+
+                        return true; // if you want to handle the touch event */
+                    case MotionEvent.ACTION_UP:
+                        Toast.makeText(MainActivity.this,"PROCESSING",Toast.LENGTH_SHORT).show();
+                        return true; // if you want to handle the touch event
+                }
+                return false;
+            }
+        });
+
+
+
+
+
+
     }
 }
